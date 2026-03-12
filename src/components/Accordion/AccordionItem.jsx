f914d5f970d7772dc1d55ed8a1bd02dabb379001
parth <parth@advant.xyz>
2026-03-12 12:48:55 +0530

    add search rerender props and context

--- src/components/Accordion/AccordionItem.jsx
+++ src/components/Accordion/AccordionItem.jsx
@@ -1,20 +1,24 @@
-import { useAccordionContext } from "./Accordion";
+import { createContext, useContext } from "react";
 
-export default function AccordionItem({ id, className, title, children }) {
-  const { openItemId, toggleItem } = useAccordionContext();
+const AccordionItemContext = createContext();
 
-  const isOpen = openItemId === id;
+export function useAccordionItemContext() {
+  const ctx = useContext(AccordionItemContext);
 
+  //if some one use outside it get error
+  if (!ctx) {
+    throw new Error(
+      "Accordion-related components must be wrapped by <Accordion.Item>.",
+    );
+  }
+
+  return ctx;
+}
+
+export default function AccordionItem({ id, className, children }) {
   return (
-    <li className={className}>
-      <h3 onClick={() => toggleItem(id)}>{title}</h3>
-      <div
-        className={
-          isOpen ? "accordion-item-content open" : "accordion-item-content"
-        }
-      >
-        {children}
-      </div>
-    </li>
+    <AccordionItemContext.Provider value={id}>
+      <li className={className}>{children}</li>
+    </AccordionItemContext.Provider>
   );
 }
---
+++ src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,33 @@
+import { useState, useRef } from "react";
+
+export default function SearchableList({ items, itemKeyFn, children }) {
+  const lastChange = useRef();
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLocaleLowerCase().includes(searchTerm.toLowerCase()),
+  );
+
+  //debouncing
+  function handleChange(event) {
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null; //remove id if timer expire
+      setSearchTerm(event.target.value);
+    }, 500);
+  }
+
+  return (
+    <div className="searchable-list">
+      <input type="search" placeholder="Search" onChange={handleChange} />
+      <ul>
+        {searchResults.map((item) => (
+          <li key={itemKeyFn(item)}>{children(item)}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
